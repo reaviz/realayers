84ca21445be902c465c58ce3dd646e28f6963776
Austin <amcdaniel2@gmail.com>
2020-11-14 11:42:00 -0500

    fix tooltip pointer

--- src/Tooltip/Tooltip.tsx
+++ src/Tooltip/Tooltip.tsx
@@ -141,6 +141,7 @@ export const Tooltip: FC<Partial<TooltipProps>> = ({
       placement={placement}
       trigger={trigger}
       followCursor={followCursor}
+      style={{ pointerEvents: 'none' }}
       open={internalVisible}
       closeOnBodyClick={closeOnBodyClick}
       closeOnEscape={closeOnEscape}
