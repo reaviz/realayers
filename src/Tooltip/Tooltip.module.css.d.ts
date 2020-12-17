4413bc1d13e7cc1ebed4d27c1313997dfa2e2057
Austin McDaniel <amcdaniel2@gmail.com>
2020-12-17 15:19:25 -0500

    fix build

--- src/Tooltip/Tooltip.module.css.d.ts
+++ src/Tooltip/Tooltip.module.css.d.ts
@@ -1,6 +1,8 @@
-export const tooltip: string
+export const tooltip: string;
+export const disablePointer: string;
 interface Namespace {
-	"tooltip": string,
+  tooltip: string;
+  disablePointer: string;
 }
-declare const tooltipModule: Namespace
-export default tooltipModule
+declare const tooltipModule: Namespace;
+export default tooltipModule;
